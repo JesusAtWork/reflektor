4754e0dec81cc5146160b416d3dc8a8a28bb5299
Alejandro J. Cura <alecu@protocultura.net>
2013-02-07 02:28:47 -0300

    Tres botones para elegir y mover los espejos, cuatro steppers en total

--- PuertaTrampa3/PuertaTrampa3.ino
+++ PuertaTrampa3/PuertaTrampa3.ino
@@ -1,13 +1,26 @@
 #include <AccelStepper.h>
 
 #define number_of_74hc595s 3 
+
 #define SER_Pin            8   //pin 14 on the 75HC595
 #define RCLK_Pin           9   //pin 12 on the 75HC595
-#define SRCLK_Pin         10   //pin 11 on the 75HC595
-#define STATUS_LED         2
+#define SRCLK_Pin          7   //pin 11 on the 75HC595
 #define FIN_CARRERA_PIN    3
+#define BOTON_1            4
+#define BOTON_2            5
+#define BOTON_3            6
+#define SENSOR0           A5
+#define SENSOR1           A4
+#define PUERTA_CERRADA  1400
+#define PUERTA_ABIERTA     0
+
+#define POCA_LUZ         100
+#define MUCHA_LUZ        150
+
 
 #define numOfRegisterPins number_of_74hc595s * 8
+#define setRegisterPin(index,value) registers[index] = value
+
 boolean registers[numOfRegisterPins];
 
 void clearRegisters(){
@@ -33,7 +46,6 @@ void writeRegisters(){
 
 }
 
-#define setRegisterPin(index,value) registers[index] = value
 
 
 class MyStepper: public AccelStepper {
@@ -50,12 +62,16 @@ class MyStepper: public AccelStepper {
       lpin3 = pin3;
       lpin4 = pin4;
     }
-   void stop()
+   void go()
    {
-     setRegisterPin(lpin1, LOW);
-     setRegisterPin(lpin2, LOW);
-     setRegisterPin(lpin3, LOW);
-     setRegisterPin(lpin4, LOW);
+     if (distanceToGo() != 0) {
+       run();
+     } else {
+       setRegisterPin(lpin1, LOW);
+       setRegisterPin(lpin2, LOW);
+       setRegisterPin(lpin3, LOW);
+       setRegisterPin(lpin4, LOW);
+     }
    }
    void step(uint8_t step)
    {
@@ -94,93 +110,164 @@ class MyStepper: public AccelStepper {
 
 };
 
-MyStepper stepper(4, 0, 1, 2, 3);
-MyStepper stepper2(4, 9, 10, 11, 12);
-int INACTIVO = 400;
-int ACTIVO = 800;
+MyStepper stepper(4, 4, 6, 5, 7);
+MyStepper stepper2(4, 0, 1, 2, 3);
+MyStepper stepper3(4, 8, 9, 10, 11);
+MyStepper stepper4(4, 12, 13, 14, 15);
+
+const int LED_GANASTE = 16;
 
 void finDeCarrera() {
-    stepper.enableOutputs();
-    stepper.setMaxSpeed(500);
-    stepper.setAcceleration(500);
-
-    stepper2.enableOutputs();
-    stepper2.setMaxSpeed(100);
-    stepper2.setAcceleration(30);
-
 
 // Ir al fin de carrera
-    stepper.setCurrentPosition(2000);
+    stepper.setCurrentPosition(PUERTA_CERRADA + 300);
     stepper2.setCurrentPosition(200);
-    stepper.moveTo(0);
+    stepper3.setCurrentPosition(200);
+    stepper4.setCurrentPosition(200);
+    stepper.moveTo(PUERTA_ABIERTA);
     stepper2.moveTo(0);
+    stepper3.moveTo(0);
+    stepper4.moveTo(0);
+    
     while(digitalRead(FIN_CARRERA_PIN) == LOW) {
-      stepper.run();
-      stepper2.run();
+      stepper.go();
+      stepper2.go();
+      stepper3.go();
+      stepper4.go();
       writeRegisters();    
-   //   clearRegisters();
-   //   writeRegisters();
     }
-    stepper.setCurrentPosition(0);
+    stepper.setCurrentPosition(PUERTA_ABIERTA);
     stepper2.setCurrentPosition(0);
+    stepper3.setCurrentPosition(0);
+    stepper4.setCurrentPosition(0);
 
 // Ir a la posicion inicial
-    stepper.moveTo(1300);
-    stepper2.moveTo(500);
+    stepper.moveTo(PUERTA_CERRADA);
+    stepper2.moveTo(50);
+    stepper3.moveTo(50);
+    stepper4.moveTo(50);
 
     while (stepper.distanceToGo() > 0) {
-      stepper.run();
-      stepper2.run();
+      stepper.go();
+      stepper2.go();
+      stepper3.go();
+      stepper4.go();
       writeRegisters();    
     }
-
-    stepper.disableOutputs();
 }
 
 void setup() {
-    Serial.begin(9600);
+//    Serial.begin(9600);
     pinMode(SER_Pin, OUTPUT);
     pinMode(RCLK_Pin, OUTPUT);
     pinMode(SRCLK_Pin, OUTPUT);
     pinMode(FIN_CARRERA_PIN, INPUT);
-    pinMode(STATUS_LED, OUTPUT);
-    digitalWrite(STATUS_LED, HIGH);
-
+    pinMode(BOTON_1, INPUT);
+    pinMode(BOTON_2, INPUT);
+    pinMode(BOTON_3, INPUT);
     clearRegisters();
     writeRegisters();
 
-//    setRegisterPin(0, HIGH);
-//    setRegisterPin(8, HIGH);
-    stepper.setEnablePin(12);
+    stepper.setMaxSpeed(5000);
+    stepper.setAcceleration(500);
+
+    stepper2.setMaxSpeed(50);
+    stepper2.setAcceleration(30);
+
+    stepper3.setMaxSpeed(50);
+    stepper3.setAcceleration(30);
+
+    stepper4.setMaxSpeed(50);
+    stepper4.setAcceleration(30);
+
     finDeCarrera();
-//    clearRegisters();
-    stepper.stop();
-    setRegisterPin(8, HIGH);
-    writeRegisters();
 }
 
+boolean abriendo_puerta = false;
+boolean estado_boton1 = false;
+boolean estado_boton2 = false;
+boolean estado_boton3 = false;
+int espejo_activo = 0;
+int count = 0;
+
+void mover_espejo(int espejo, int direccion) {
+  if (espejo == 0) {
+      stepper2.move(direccion);
+  } else if (espejo == 1) {
+      stepper3.move(direccion);
+  } else {
+      stepper4.move(direccion);
+  }
+}
 
 void loop() {  
-    int ldr = analogRead(A5); 
+    int sensor1 = analogRead(SENSOR1); 
     
-    if (ldr > ACTIVO) {
-        
-        stepper.disableOutputs();
-    }
-    
-    if (ldr < INACTIVO) {
+    if (abriendo_puerta) {
+      if(digitalRead(FIN_CARRERA_PIN) == HIGH) {
+        // llegamos al fin de la carrera
+        abriendo_puerta = false;
+        stepper.setMaxSpeed(100);
+        stepper.setAcceleration(20);
+        stepper.setCurrentPosition(PUERTA_ABIERTA);
+      }
       
-    }
-    //int sensorValue = digitalRead(3);
-    //Serial.println(sensorValue);
+    } else {
 
-    if (stepper2.distanceToGo() == 0) {
-     //stepper2.setCurrentPosition(200);
-     //stepper2.moveTo(0);
-     //setRegisterPin(8, LOW);
-     stepper2.stop();
+      if (sensor1 > MUCHA_LUZ && digitalRead(FIN_CARRERA_PIN) == LOW) {
+        stepper.setMaxSpeed(5000);
+        stepper.setAcceleration(500);
+        abriendo_puerta = true;
+        stepper.move(-PUERTA_CERRADA * 2);
+      }
+      
+      if (sensor1 < POCA_LUZ) {
+        stepper.moveTo(PUERTA_CERRADA);      
+      }
+    }
+    
+    int sensor2 = analogRead(SENSOR0);
+    if (sensor2 > MUCHA_LUZ) {
+        setRegisterPin(LED_GANASTE, HIGH);      
+    } else {
+        setRegisterPin(LED_GANASTE, LOW);
+    }  
+ 
+    if (digitalRead(BOTON_1) == HIGH) {
+        estado_boton1 = true;
     }
 
-    stepper2.run();
+    if (digitalRead(BOTON_2) == HIGH) {
+        estado_boton2 = true;
+    }
+
+    if (digitalRead(BOTON_3) == HIGH) {
+        estado_boton3 = true;
+    }
+ 
+    count += 1;
+    if (true || count % 1) {
+
+      if (estado_boton1) {
+        espejo_activo = (espejo_activo + 1) % 3;
+      }
+      
+      if (estado_boton2 && !estado_boton3) {
+        mover_espejo(espejo_activo, -4);
+      }
+
+      if (estado_boton3 && !estado_boton2) {
+        mover_espejo(espejo_activo, 4);
+      }
+      
+      estado_boton1 = false;
+      estado_boton2 = false;
+      estado_boton3 = false;
+    }
+    
+    stepper.go();
+    stepper2.go();
+    stepper3.go();
+    stepper4.go();
     writeRegisters();
 }
