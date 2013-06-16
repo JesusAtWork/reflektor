1a41f876fd0e1852d1fe6ec1024c943d3730ec14
Alejandro J. Cura <alecu@protocultura.net>
2013-06-16 01:04:41 -0300

    Un poquito de reformateo

--- Stepper595/Stepper595.cpp
+++ Stepper595/Stepper595.cpp
@@ -2,22 +2,22 @@
 
 boolean registers[numOfRegisterPins];
 
-void clearRegisters(){
-  for(int i = numOfRegisterPins - 1; i >=  0; i--){
-     registers[i] = LOW;
-  }
-} 
+void clearRegisters() {
+    for(int i = numOfRegisterPins - 1; i >=  0; i--) {
+        registers[i] = LOW;
+    }
+}
 
-void writeRegisters(){
-  digitalWrite(RCLK_Pin, LOW);
+void writeRegisters() {
+    digitalWrite(RCLK_Pin, LOW);
 
-  for(int i = numOfRegisterPins - 1; i >=  0; i--){
-    digitalWrite(SRCLK_Pin, LOW);
-    int val = registers[i];
-    digitalWrite(SER_Pin, val);
-    digitalWrite(SRCLK_Pin, HIGH);
-  }
-  digitalWrite(RCLK_Pin, HIGH);
+    for(int i = numOfRegisterPins - 1; i >=  0; i--) {
+        digitalWrite(SRCLK_Pin, LOW);
+        int val = registers[i];
+        digitalWrite(SER_Pin, val);
+        digitalWrite(SRCLK_Pin, HIGH);
+    }
+    digitalWrite(RCLK_Pin, HIGH);
 }
 
 Stepper::Stepper(uint8_t pins, uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4) : AccelStepper(pins, pin1, pin2, pin3, pin4) {
@@ -31,66 +31,58 @@ Stepper::Stepper(uint8_t pins, uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t
 
 void Stepper::go(unsigned long current_time)
 {
-  if (distanceToGo() != 0) {
-    stop_time = current_time + delay_before_fullstop;
-    run();
-  } else {
-    if (current_time >= stop_time) {
-      fullstop();
+    if (distanceToGo() != 0) {
+        stop_time = current_time + delay_before_fullstop;
+        run();
+    } else {
+        if (current_time >= stop_time) {
+            fullstop();
+        }
     }
-  }
+}
+
+void Stepper::stop() {
+    move(0);
 }
 
 void Stepper::fullstop() {
-   setRegisterPin(lpin1, LOW);
-   setRegisterPin(lpin2, LOW);
-   setRegisterPin(lpin3, LOW);
-   setRegisterPin(lpin4, LOW);
+    setRegisterPin(lpin1, LOW);
+    setRegisterPin(lpin2, LOW);
+    setRegisterPin(lpin3, LOW);
+    setRegisterPin(lpin4, LOW);
 }
 
 void Stepper::step(uint8_t step)
 {
-switch (step & 0x3)
-{
-case 0:    // 1010
-    setRegisterPin(lpin1, HIGH);
-    setRegisterPin(lpin2, LOW);
-    setRegisterPin(lpin3, HIGH);
-    setRegisterPin(lpin4, LOW);
-    break;
+    switch (step & 0x3)
+    {
+    case 0:    // 1010
+        setRegisterPin(lpin1, HIGH);
+        setRegisterPin(lpin2, LOW);
+        setRegisterPin(lpin3, HIGH);
+        setRegisterPin(lpin4, LOW);
+        break;
 
-case 1:    // 0110
-    setRegisterPin(lpin1, LOW);
-    setRegisterPin(lpin2, HIGH);
-    setRegisterPin(lpin3, HIGH);
-    setRegisterPin(lpin4, LOW);
-    break;
+    case 1:    // 0110
+        setRegisterPin(lpin1, LOW);
+        setRegisterPin(lpin2, HIGH);
+        setRegisterPin(lpin3, HIGH);
+        setRegisterPin(lpin4, LOW);
+        break;
 
-case 2:    //0101
-    setRegisterPin(lpin1, LOW);
-    setRegisterPin(lpin2, HIGH);
-    setRegisterPin(lpin3, LOW);
-    setRegisterPin(lpin4, HIGH);
-    break;
+    case 2:    //0101
+        setRegisterPin(lpin1, LOW);
+        setRegisterPin(lpin2, HIGH);
+        setRegisterPin(lpin3, LOW);
+        setRegisterPin(lpin4, HIGH);
+        break;
 
 
-case 3:    //1001
-    setRegisterPin(lpin1, HIGH);
-    setRegisterPin(lpin2, LOW);
-    setRegisterPin(lpin3, LOW);
-    setRegisterPin(lpin4, HIGH);
-    break;
+    case 3:    //1001
+        setRegisterPin(lpin1, HIGH);
+        setRegisterPin(lpin2, LOW);
+        setRegisterPin(lpin3, LOW);
+        setRegisterPin(lpin4, HIGH);
+        break;
+    }
 }
-}
-/*
-StepperManager::StepperManager(int ser_pin, int rclk_pin, int srclk_pin){
-  this->ser_pin = ser_pin;
-  this->rclk_pin = rclk_pin;
-  this->srclk_pin = srclk_pin;
-}
-
-void StepperManager::set_steppers(Stepper *stepper_list, int num_elements) {
-  this->stepper_list = stepper_list;
-  this->num_elements = num_elements;
-}
-*/
--- Stepper595/Stepper595.h
+++ Stepper595/Stepper595.h
@@ -4,7 +4,7 @@
 #define RCLK_Pin           9   //pin 12 on the 75HC595
 #define SRCLK_Pin          7   //pin 11 on the 75HC595
 
-#define number_of_74hc595s 3 
+#define number_of_74hc595s 3
 #define numOfRegisterPins number_of_74hc595s * 8
 #define setRegisterPin(index,value) registers[index] = value
 
@@ -17,32 +17,20 @@ void clearRegisters();
 void writeRegisters();
 
 class Stepper: public AccelStepper {
-  private:
+private:
     void fullstop();
     unsigned long stop_time;
 
-  protected:
+protected:
     uint8_t lpin1;
     uint8_t lpin2;
     uint8_t lpin3;
     uint8_t lpin4;
-    
-  public:
+
+public:
     int delay_before_fullstop;
     Stepper(uint8_t pins, uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4);
     void go(unsigned long current_time);
     void step(uint8_t step);
     void stop();
 };
-
-/*
-class StepperManager {
-  Stepper *stepper_list;
-  int num_elements;
-  int ser_pin, rclk_pin, srclk_pin;
-  public:
-    StepperManager(int ser_pin, int rclk_pin, int srclk_pin);
-    void set_steppers(Stepper *stepper_list, int num_elements);
-    void go();
-};
-*/
